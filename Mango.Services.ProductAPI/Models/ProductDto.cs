be15498e0e8ddebc5d6d51f6e889d69012d1b638
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-01 13:07:42 -0500

    Section 3 - Create DTO's for product API

---
+++ Mango.Services.ProductAPI/Models/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ProductAPI.Models.Dtos
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
