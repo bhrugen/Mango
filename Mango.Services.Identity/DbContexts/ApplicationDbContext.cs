34f7affaa3bc7090bb215d3a0050bf59e32f1cb0
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-02 09:56:36 -0500

    Section 5 - DbContext for Identity Server

---
+++ Mango.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+
+    }
+}
