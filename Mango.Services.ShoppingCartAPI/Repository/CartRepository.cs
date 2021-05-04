9e239297bd660b088748fe4272a6747b6e79014f
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-04 15:33:39 -0500

    Section 7 - Cart Repository Interface for Shopping Cart API

---
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -0,0 +1,31 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        public async Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartDto> GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveFromCart(int cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartDto> GetCartByUserId(string userId);
+        Task<CartDto> CreateUpdateCart(CartDto cartDto);
+        Task<bool> RemoveFromCart(int cartDetailsId);
+        Task<bool> ClearCart(string userId);
+    }
+}
