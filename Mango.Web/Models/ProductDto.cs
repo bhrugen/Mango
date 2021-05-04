8681c963f63201a662f07dce3e267c2b637c2a52
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-04 12:00:07 -0500

    Section 6 - Details View

--- Mango.Web/Models/ProductDto.cs
+++ Mango.Web/Models/ProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace Mango.Web.Models
 {
     public class ProductDto
     {
+        public ProductDto()
+        {
+            Count = 1;
+        }
         public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
-        public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        public string CategoryName { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; }
     }
 }
