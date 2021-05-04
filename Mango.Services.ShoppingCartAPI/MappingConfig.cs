882d9992474d4f34a33b7c6e88fb61ae8b925917
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-04 15:28:02 -0500

    Section 7 - Mapping Configuration for Shopping Cart

--- Mango.Services.ShoppingCartAPI/MappingConfig.cs
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +14,10 @@ namespace Mango.Services.ShoppingCartAPI
         {
             var mappingConfig = new MapperConfiguration(config =>
             {
-                //config.CreateMap<ProductDto, Product>();
-                //config.CreateMap<Product, ProductDto>();
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();
             });
 
             return mappingConfig;
