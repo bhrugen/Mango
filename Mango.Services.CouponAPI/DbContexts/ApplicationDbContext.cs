b180b0850432356560fde4f79bccbda0a62fa98e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-05 16:52:34 -0500

    Section 10 - Setup Coupon API Project

---
+++ Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+
+    }
+}
---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<ProductDto, Product>().ReverseMap();
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                //config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
