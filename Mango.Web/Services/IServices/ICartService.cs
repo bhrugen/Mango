d1f71881bf5f80fdaa41ff23d4b545d41b910abe
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-04 20:47:49 -0500

    Section 8 - Define Cart Service Interface

---
+++ Mango.Web/Services/IServices/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using Mango.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface ICartService
+    {
+        Task<T> GetCartByUserIdAsnyc<T>(string userId, string token = null);
+        Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
+        Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
+        Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
+
+    }
+}
