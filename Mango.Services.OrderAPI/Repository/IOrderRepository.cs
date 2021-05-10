c363c88861fdefcde8e38f280588294ffa2d5d26
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-10 15:33:46 -0500

    Section 14 - Order Repository Interface

---
+++ Mango.Services.OrderAPI/Repository/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Services.OrderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader orderHeader);
+        Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid);
+    }
+}
---
+++ Mango.Services.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Mango.Services.OrderAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public Task<bool> AddOrder(OrderHeader orderHeader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
