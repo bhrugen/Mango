4fcf0b6f47ed254a1977efb77709dcea3fdabb99
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-10 15:45:27 -0500

    Section 14 - Configure DbContext for Order Repository

--- Mango.Services.OrderAPI/Startup.cs
+++ Mango.Services.OrderAPI/Startup.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Mango.Services.OrderAPI.DbContexts;
+using Mango.Services.OrderAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,7 +39,12 @@ namespace Mango.Services.OrderAPI
             //IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
             //services.AddSingleton(mapper);
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-            //services.AddScoped<ICouponRepository, CouponRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddSingleton(new OrderRepository(optionBuilder.Options));
+
             services.AddControllers();
 
             services.AddAuthentication("Bearer")
