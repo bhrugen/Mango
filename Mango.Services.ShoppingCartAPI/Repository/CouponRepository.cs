693ee36e8ceb2560eb43270dbb7d4c9eedfabb9d
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-11 11:22:42 -0500

    Section 15 - Implement Coupon Repository

---
+++ Mango.Services.ShoppingCartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly HttpClient client;
+
+        public CouponRepository(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<CouponDto> GetCoupon(string couponName)
+        {
+            var response = await client.GetAsync($"/api/coupon/{couponName}");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if (resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(resp.Result));
+            }
+            return new CouponDto();
+        }
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCoupon(string couponName);
+    }
+}
