4aebec8052ce48bc0c4ce570f7d9d220002e2422
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-11 13:40:48 -0500

    Section 16 - Create Payment Processor Class library

---
+++ PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PaymentProcessor
+{
+    public class ProcessPayment : IProcessPayment
+    {
+        public bool PaymentProcessor()
+        {
+            //implement custom logic and get card details etc
+            return true;
+        }
+    }
+}
