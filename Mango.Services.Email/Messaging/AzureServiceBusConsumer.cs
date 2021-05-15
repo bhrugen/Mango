14cdc1a61b30a9a4b8f6a6a116878f32b8e7b41f
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-15 18:06:38 -0500

    Section 17 - Azure Consumer Bus for Email Service

--- Mango.Services.Email/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.Email/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,4 @@
 ﻿using Azure.Messaging.ServiceBus;
-using Mango.Services.OrderAPI.Messages;
-using Mango.Services.OrderAPI.Models;
-using Mango.Services.OrderAPI.Repository;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,60 +6,49 @@ using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Mango.MessageBus;
+using Mango.Services.Email.Repository;
+using Newtonsoft.Json;
+using Mango.Services.Email.Messages;
 
 namespace Mango.Services.Email.Messaging
 {  
     public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string serviceBusConnectionString;
-        private readonly string subscriptionCheckOut;
-        private readonly string checkoutMessageTopic;
-        private readonly string orderPaymentProcessTopic;
+        private readonly string subscriptionEmail;
         private readonly string orderUpdatePaymentResultTopic;
 
-        private readonly OrderRepository _orderRepository;
+        private readonly EmailRepository _emailRepo;
 
-        private ServiceBusProcessor checkOutProcessor;
         private ServiceBusProcessor orderUpdatePaymentStatusProcessor;
 
         private readonly IConfiguration _configuration;
-        private readonly IMessageBus _messageBus;
 
-        public AzureServiceBusConsumer(OrderRepository orderRepository, IConfiguration configuration, IMessageBus messageBus)
+        public AzureServiceBusConsumer(EmailRepository emailRepo, IConfiguration configuration)
         {
-            _orderRepository = orderRepository;
+            _emailRepo = emailRepo;
             _configuration = configuration;
-            _messageBus = messageBus;
 
             serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
-            subscriptionCheckOut = _configuration.GetValue<string>("SubscriptionCheckOut");
-            checkoutMessageTopic = _configuration.GetValue<string>("CheckoutMessageTopic");
-            orderPaymentProcessTopic = _configuration.GetValue<string>("OrderPaymentProcessTopics");
+            subscriptionEmail = _configuration.GetValue<string>("SubscriptionName");
             orderUpdatePaymentResultTopic = _configuration.GetValue<string>("OrderUpdatePaymentResultTopic");
 
 
             var client = new ServiceBusClient(serviceBusConnectionString);
 
-            checkOutProcessor = client.CreateProcessor(checkoutMessageTopic);
-            orderUpdatePaymentStatusProcessor = client.CreateProcessor(orderUpdatePaymentResultTopic, subscriptionCheckOut);
+            orderUpdatePaymentStatusProcessor = client.CreateProcessor(orderUpdatePaymentResultTopic, subscriptionEmail);
         }
 
         public async Task Start()
         {
-            checkOutProcessor.ProcessMessageAsync += OnCheckOutMessageReceived;
-            checkOutProcessor.ProcessErrorAsync += ErrorHandler;
-            await checkOutProcessor.StartProcessingAsync();
-
+          
             orderUpdatePaymentStatusProcessor.ProcessMessageAsync += OnOrderPaymentUpdateReceived;
             orderUpdatePaymentStatusProcessor.ProcessErrorAsync += ErrorHandler;
             await orderUpdatePaymentStatusProcessor.StartProcessingAsync();
         }
         public async Task Stop()
         {
-            await checkOutProcessor.StopProcessingAsync();
-            await checkOutProcessor.DisposeAsync();
-
+            
             await orderUpdatePaymentStatusProcessor.StopProcessingAsync();
             await orderUpdatePaymentStatusProcessor.DisposeAsync();
         }
@@ -78,56 +63,10 @@ namespace Mango.Services.Email.Messaging
             var message = args.Message;
             var body = Encoding.UTF8.GetString(message.Body);
 
-            CheckoutHeaderDto checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
-
-            OrderHeader orderHeader = new()
-            {
-                UserId = checkoutHeaderDto.UserId,
-                FirstName = checkoutHeaderDto.FirstName,
-                LastName = checkoutHeaderDto.LastName,
-                OrderDetails = new List<OrderDetails>(),
-                CardNumber = checkoutHeaderDto.CardNumber,
-                CouponCode = checkoutHeaderDto.CouponCode,
-                CVV = checkoutHeaderDto.CVV,
-                DiscountTotal = checkoutHeaderDto.DiscountTotal,
-                Email = checkoutHeaderDto.Email,
-                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
-                OrderTime = DateTime.Now,
-                OrderTotal = checkoutHeaderDto.OrderTotal,
-                PaymentStatus = false,
-                Phone = checkoutHeaderDto.Phone,
-                PickupDateTime = checkoutHeaderDto.PickupDateTime
-            };
-            foreach(var detailList in checkoutHeaderDto.CartDetails)
-            {
-                OrderDetails orderDetails = new()
-                {
-                    ProductId = detailList.ProductId,
-                    ProductName = detailList.Product.Name,
-                    Price = detailList.Product.Price,
-                    Count = detailList.Count
-                };
-                orderHeader.CartTotalItems += detailList.Count;
-                orderHeader.OrderDetails.Add(orderDetails);
-            }
-
-            await _orderRepository.AddOrder(orderHeader);
-
-
-            PaymentRequestMessage paymentRequestMessage = new()
-            {
-                Name = orderHeader.FirstName + " " + orderHeader.LastName,
-                CardNumber = orderHeader.CardNumber,
-                CVV = orderHeader.CVV,
-                ExpiryMonthYear = orderHeader.ExpiryMonthYear,
-                OrderId = orderHeader.OrderHeaderId,
-                OrderTotal = orderHeader.OrderTotal,
-                Email=orderHeader.Email
-            };
-
+            UpdatePaymentResultMessage objMessage = JsonConvert.DeserializeObject<UpdatePaymentResultMessage>(body);
             try
             {
-                await _messageBus.PublishMessage(paymentRequestMessage, orderPaymentProcessTopic);
+                await _emailRepo.SendAndLogEmail(objMessage);
                 await args.CompleteMessageAsync(args.Message);
             }
             catch(Exception e)
