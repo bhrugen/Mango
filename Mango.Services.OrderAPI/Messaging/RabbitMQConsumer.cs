30cf52aa225d96bf86958ab6e8b8cec724237464
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-30 09:34:57 -0500

    Section 19 - Setup RabbitMQ Consumer Part 1

---
+++ Mango.Services.OrderAPI/Messaging/RabbitMQConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.OrderAPI.Repository;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mango.Services.OrderAPI.Messaging
+{
+    public class RabbitMQConsumer : BackgroundService
+    {
+        private readonly OrderRepository _orderRepository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQConsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, arguments: null);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
