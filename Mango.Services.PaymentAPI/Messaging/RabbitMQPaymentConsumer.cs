07ac113247e60f4aada0787d4160168ef4379566
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-30 10:21:42 -0500

    Section 19 - Consume message from Order API

---
+++ Mango.Services.PaymentAPI/Messaging/RabbitMQPaymentConsumer.cs
@@ -0,0 +1,87 @@
+﻿using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.RabbitMQSender;
+using Mango.Services.OrderAPI.Repository;
+using Mango.Services.PaymentAPI.Messages;
+using Mango.Services.PaymentAPI.RabbitMQSender;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using PaymentProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mango.Services.PaymentAPI.Messaging
+{
+    public class RabbitMQPaymentConsumer : BackgroundService
+    {
+        
+        private IConnection _connection;
+        private IModel _channel;
+        private readonly IRabbitMQPaymentMessageSender _rabbitMQPaymentMessageSender;
+        private readonly IProcessPayment _processPayment;
+
+        public RabbitMQPaymentConsumer(IRabbitMQPaymentMessageSender rabbitMQPaymentMessageSender,
+            IProcessPayment processPayment)
+        {
+            _processPayment = processPayment;
+            _rabbitMQPaymentMessageSender = rabbitMQPaymentMessageSender;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest"
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "orderpaymentprocesstopic", false, false, false, arguments: null);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                PaymentRequestMessage paymentRequestMessage = JsonConvert.DeserializeObject<PaymentRequestMessage>(content);
+                HandleMessage(paymentRequestMessage).GetAwaiter().GetResult();
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+            _channel.BasicConsume("checkoutqueue", false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task HandleMessage(PaymentRequestMessage paymentRequestMessage)
+        {
+            var result = _processPayment.PaymentProcessor();
+
+            UpdatePaymentResultMessage updatePaymentResultMessage = new()
+            {
+                Status = result,
+                OrderId = paymentRequestMessage.OrderId,
+                Email = paymentRequestMessage.Email
+            };
+
+
+            try
+            {
+
+               // await _messageBus.PublishMessage(updatePaymentResultMessage, orderupdatepaymentresulttopic);
+               // await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
