d2f9b2195e8679227d56e0ce69487047250b7331
Bhrugen Patel <bhrugen1990@gmail.com>
2021-05-30 09:01:58 -0500

    Section 19 - Add RabbitMQ Nuget to Project

---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/IRabbitMQCartMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using Mango.MessageBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    interface IRabbitMQCartMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage, String queueName);
+    }
+}
---
+++ Mango.Services.ShoppingCartAPI/RabbitMQSender/RabbitMQCartMessageSender.cs
@@ -0,0 +1,16 @@
+﻿using Mango.MessageBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mango.Services.ShoppingCartAPI.RabbitMQSender
+{
+    public class RabbitMQCartMessageSender : IRabbitMQCartMessageSender
+    {
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
