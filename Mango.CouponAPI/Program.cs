f01aaccdde8c7deb0688244434da323df645bbe8
Bhrugen Patel <bhrugen7@gmail.com>
2026-05-22 12:30:13 -0500

    Section 2 - Add Scalar UI

--- Mango.CouponAPI/Program.cs
+++ Mango.CouponAPI/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
