c36095506d4aa9452eab31b746c7d1c334210009
Bhrugen Patel <bhrugen7@gmail.com>
2026-05-22 12:13:18 -0500

    Section 2 - Create web and API project

---
+++ Mango.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Mango.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Mango.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
