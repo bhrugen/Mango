66fbde502173628ed2b3b492348aa925852c809c
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-20 10:45:15 -0500

    Section 3 - Generic Response DTO

---
+++ Mango.Serives.Shared/Models/ResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mango.Serives.Shared.Models
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string? ErrorMessage { get; set; }
+    }
+}
