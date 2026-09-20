3e81fbf6fb163a8f38ce41bdca3365a2769508aa
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-20 14:15:50 -0500

    Section 4 - Base Service Implementation

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -1,6 +1,62 @@
-﻿namespace Mango.Web.Service
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using System.Text;
+using System.Text.Json;
+
+namespace Mango.Web.Service
 {
-    public class BaseService
+    public class BaseService : IBaseService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new();
+
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(requestDto.Url!);
+
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonSerializer.Serialize(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+                message.Method = requestDto.ApiType switch
+                {
+                    SD.ApiType.POST => HttpMethod.Post,
+                    SD.ApiType.PUT => HttpMethod.Put,
+                    SD.ApiType.DELETE => HttpMethod.Delete,
+                    _ => HttpMethod.Get
+                };
+
+                HttpResponseMessage? apiResponse = null;
+                apiResponse = await client.SendAsync(message);
+
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonSerializer.Deserialize<ResponseDto>(apiContent, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+                return apiResponseDto!;
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    IsSuccess = false,
+                    ErrorMessage = ex.Message
+                };
+                return dto;
+            }
+        }
     }
 }
