53bacb403eca09db2c9334cfda0a7013b5a5107f
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-21 13:45:33 -0500

    Section 5 - Create Basic Endpoints

---
+++ Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Serives.Shared.Models;
+using Mango.Services.AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.AuthAPI.Controllers
+{
+    public class AuthAPIController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private const string CustomerRole = "CUSTOMER";
+        private const string AdminRole = "ADMIN";
+        private readonly ResponseDto _responseDto;
+
+
+        public AuthAPIController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _responseDto = new ResponseDto();
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Register([FromBody] RegisterationRequestDto model)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto model)
+        {
+            return View();
+        }
+    }
+}
