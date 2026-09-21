21dc34b0567de0eeefbf9df5420def839f45568f
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-20 20:13:11 -0500

    Section 4 - Setup Templates for UI

---
+++ Mango.Web/Controllers/CouponController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+        public async Task<IActionResult> CouponIndex()
+        {
+            ResponseDto? responseDto = await _couponService.GetAllCouponsAsync();
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                // Handle the successful response
+                List<CouponDto>? coupons = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(responseDto.Result));
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            
+            return View();
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            
+            return View();
+        }
+    }
+}
