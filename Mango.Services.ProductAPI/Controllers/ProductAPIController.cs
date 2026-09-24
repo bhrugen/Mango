55226119fe163e0ccbdd766637f985cb19bc06f7
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-24 11:16:47 -0500

    Section 7 - Assignment Setup

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+    }
+}
