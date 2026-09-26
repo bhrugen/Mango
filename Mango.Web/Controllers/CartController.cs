3a1096ec6d0b7f507b619f712df63e7f164f266e
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-26 13:11:55 -0500

    Section 10 - Cart UI in Action

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await LoadCartByLoggedInUser());
+        }
+        
+        private async Task<CartDto> LoadCartByLoggedInUser()
+        {
+            var UserId = User.Claims.Where(u => u.Type == "sub").FirstOrDefault()?.Value;
+            if(string.IsNullOrEmpty(UserId))
+            {
+                return new CartDto();
+            }
+            ResponseDto? responseDto = await _cartService.GetCartByUserIdAsync(UserId);
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(responseDto.Result));
+                return cartDto;
+            }
+
+            return new CartDto();
+        }
+    }
+}
