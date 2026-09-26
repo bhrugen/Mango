2da5f64598aad969fde581758caa6554bc265cb8
Bhrugen Patel <bhrugen7@gmail.com>
2026-09-26 14:55:42 -0500

    Section 10 - Bind Details in Cart

--- Mango.Web/Models/CartDto.cs
+++ Mango.Web/Models/CartDto.cs
@@ -1,8 +1,16 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class CartDto
     {
         public CartHeaderDto? CartHeader { get; set; }
         public IEnumerable<CartDetailsDto>? CartDetails { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public string? Email { get; set; }
     }
 }
